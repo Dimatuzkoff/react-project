866f2cb252a458400b80dc9e9243793370e55122
Dimatuzkoff <dimatuzkoff@gmail.com>
2025-06-05 17:02:25 +0300

    feat(scrum-3): created routeConfig and pages components

---
+++ src/app/config/route/routeConfig.ts
@@ -0,0 +1,12 @@
+export const routeConfig = {
+    home: '/',
+    contact: '/contact',
+    about: '/about',
+    singUp: '/singUp',
+    wishlist: '/wishlist',
+    cart: '/cart',
+    checkOut: '/checkout',
+    account: '/account',
+    product: '/product/:id',
+    notFound: '*',
+};
---
+++ src/pages/checkout/ui/CheckoutPage.tsx
@@ -0,0 +1,7 @@
+export const CheckoutPage = () => {
+    return (
+        <div>
+            <h1>CheckoutPage</h1>
+        </div>
+    );
+};
