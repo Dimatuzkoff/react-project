aa9131dbaf832150b854c81dcb9e14f2b81cffe9
Dimatuzkoff <dimatuzkoff@gmail.com>
2025-06-05 17:39:52 +0300

    feat(scrum-3): created AppRouter and some constants for routes

---
+++ src/shared/libs/constants/routes/routes.ts
@@ -0,0 +1,21 @@
+import { routeConfig } from '@/app/config/route/routeConfig';
+
+export const getHomeRoute = () => routeConfig.home;
+
+export const getContactRoute = () => routeConfig.contact;
+
+export const getAboutRoute = () => routeConfig.about;
+
+export const getSingUpRoute = () => routeConfig.singUp;
+
+export const getWishlistRoute = () => routeConfig.wishlist;
+
+export const getCartRoute = () => routeConfig.cart;
+
+export const getCheckoutRoute = () => routeConfig.checkout;
+
+export const getAccountRoute = () => routeConfig.account;
+
+export const getProductRoute = () => routeConfig.product;
+
+export const getNotFoundRoute = () => routeConfig.notFound;
