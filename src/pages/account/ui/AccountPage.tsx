2afe53269c5b822173b2dd0594b3d13bce2fee10
Dimatuzkoff <dimatuzkoff@gmail.com>
2025-06-06 14:16:25 +0300

    feat(scrum-1): created BaseLayout and added some styles for each page

--- src/pages/account/ui/AccountPage.tsx
+++ src/pages/account/ui/AccountPage.tsx
@@ -1,7 +1,11 @@
+//styles
+import styles from './AccountPage.module.scss';
 export const AccountPage = () => {
     return (
-        <div>
-            <h1>AccountPage</h1>
-        </div>
+        <>
+            <section className={styles.wrapperAccountPage}>
+                <h1>AccountPage</h1>
+            </section>
+        </>
     );
 };
