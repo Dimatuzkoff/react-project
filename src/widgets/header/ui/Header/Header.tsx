cc1f0b675c1ce76c5462291a0060f94d902b82bb
Dimatuzkoff <dimatuzkoff@gmail.com>
2025-06-08 23:09:39 +0300

    feat(scrum-1): added header

--- src/widgets/header/ui/Header/Header.tsx
+++ src/widgets/header/ui/Header/Header.tsx
@@ -1,12 +1,43 @@
 //react
 import type { FC } from 'react';
+import { NavLink } from 'react-router-dom';
 //styles
 import styles from './Header.module.scss';
+//ui
+import { HeaderTools } from '../HeaderTools/HeaderTools';
+import { HeaderNavigation } from '../HeaderNavigation/HeaderNavigation';
+//constants
+import { getHomeRoute } from '@/shared/libs/constants/routes/routes';
 
 interface HeaderProps {
     [key: string]: unknown;
 }
 
 export const Header: FC<HeaderProps> = ({}) => {
-    return <div className={styles.header}></div>;
+    console.log('render Header');
+
+    return (
+        <>
+            <section className={styles.wrapperHeader}>
+                <div className={styles.discountOffer}>
+                    <p>
+                        Літній розпродаж на всі купальники та безкоштовна
+                        експрес-доставка - ЗНИЖКА 50%!
+                    </p>
+                </div>
+                <div className={styles.header}>
+                    <NavLink to={getHomeRoute()}>
+                        <h1>Exclusive</h1>
+                    </NavLink>
+                    <div className={styles.headerMenuDesktop}>
+                        <HeaderNavigation />
+                    </div>
+                    <HeaderTools />
+                </div>
+                <div className={styles.headerMenuMobile}>
+                    <HeaderNavigation />
+                </div>
+            </section>
+        </>
+    );
 };
