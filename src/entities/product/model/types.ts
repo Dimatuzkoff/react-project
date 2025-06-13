721b01f03c6828f3163738025b4ab9ed632f1efa
DariaZhur <dariazhur89@gmail.com>
2025-06-13 13:39:33 +0300

    feat(scrum-4): fix Product type

--- src/entities/product/model/types.ts
+++ src/entities/product/model/types.ts
@@ -11,25 +11,29 @@ export interface Product {
     brand: string;
     sku: string;
     weight: number;
-    dimensions: {
-        width: number;
-        height: number;
-        depth: number;
-    };
+    dimensions: Dimensions;
     warrantyInformation: string;
     shippingInformation: string;
     availabilityStatus: string;
     reviews: Review[];
     returnPolicy: string;
     minimumOrderQuantity: number;
-    meta: {
-        createdAt: string;
-        updatedAt: string;
-        barcode: string;
-        qrCode: string;
-    };
+    meta: ProductMeta;
     images: string[];
     thumbnail: string;
+}
+
+export interface Dimensions {
+    width: number;
+    height: number;
+    depth: number;
+}
+
+export interface ProductMeta {
+    createdAt: string;
+    updatedAt: string;
+    barcode: string;
+    qrCode: string;
 }
 
 export interface Review {
