52bf98e9596c7c7fa3da117240c2961fd7c07cef
Dimatuzkoff <dimatuzkoff@gmail.com>
2025-06-16 10:24:45 +0300

    feat(scrum-1): added info to footer

--- src/widgets/header/ui/HeaderTools/HeaderTools.tsx
+++ src/widgets/header/ui/HeaderTools/HeaderTools.tsx
@@ -5,27 +5,27 @@ import styles from './HeaderTools.module.scss';
 //assets
 import Cart from '@/shared/libs/assets/svg/icons/cart.svg';
 import Wishlist from '@/shared/libs/assets/svg/icons/wishlist.svg';
-// import Search from '@/shared/libs/assets/svg/icons/search.svg';
+import Search from '@/shared/libs/assets/svg/icons/search.svg';
 //constants
 import {
     getWishlistRoute,
     getCartRoute,
 } from '@/shared/libs/constants/routes/routes';
 //ui
-// import { Input } from '@/shared/ui/input';
+import { Input } from '@/shared/ui/input';
 
 export const HeaderTools = () => {
     return (
         <>
             <div className={styles.headerTools}>
-                {/* <div className={styles.inputWrapper}>
+                <div className={styles.inputWrapper}>
                     <Input
                         type="search"
                         placeholder="Що ви шукаєте?"
                         iconAfter={<img src={Search} alt="cart" />}
                         isQuiet
                     />
-                </div> */}
+                </div>
 
                 <NavLink to={getWishlistRoute()}>
                     <img src={Wishlist} alt="wishlist" />
