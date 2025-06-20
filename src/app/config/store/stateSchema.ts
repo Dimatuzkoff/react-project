5fde54ac6e3ba227382b48bdaaf02190d9e682df
Dimatuzkoff <dimatuzkoff@gmail.com>
2025-06-20 19:08:39 +0300

    feat(scrum-8): added store for wishlist

--- src/app/config/store/stateSchema.ts
+++ src/app/config/store/stateSchema.ts
@@ -1,5 +1,7 @@
 import type { CartStateType } from '@/features/cart/model/types/cartTypes';
+import type { WishlistStateType } from '@/features/wishlist/model/types/wishlistTypes';
 
 export type StateSchema = {
     cart: CartStateType;
+    wishlist: WishlistStateType;
 };
