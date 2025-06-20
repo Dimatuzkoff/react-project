3ed33f46508382f6bf4e4ccc2bc13364a83f92a1
Dimatuzkoff <dimatuzkoff@gmail.com>
2025-06-20 19:08:39 +0300

    feat(scrum-8): added store for wishlist

--- src/app/config/store/createReduxStore.ts
+++ src/app/config/store/createReduxStore.ts
@@ -1,9 +1,11 @@
 import { combineReducers, createStore } from 'redux';
 import { cartReducer } from '@/features/cart/model/reducer/cartReducer';
+import { wishlistReducer } from '@/features/wishlist/model/reducer/wishlistReducer';
 
 export const createReduxStore = () => {
     const reducers = {
         cart: cartReducer,
+        wishlist: wishlistReducer,
     };
 
     const rootReducer = combineReducers(reducers);
