a11bf1b83b602e8777ee5e40a2ffff555aa68adc
Yaroslav Kuibida <126953294+FallenSpacee@users.noreply.github.com>
2025-06-24 21:17:02 +0300

    Revert "feat(scrum-8): added store for wishlist"
    This reverts commit 3ed33f46508382f6bf4e4ccc2bc13364a83f92a1.

--- src/app/config/store/createReduxStore.ts
+++ src/app/config/store/createReduxStore.ts
@@ -1,11 +1,9 @@
 import { combineReducers, createStore } from 'redux';
 import { cartReducer } from '@/features/cart/model/reducer/cartReducer';
-import { wishlistReducer } from '@/features/wishlist/model/reducer/wishlistReducer';
 
 export const createReduxStore = () => {
     const reducers = {
         cart: cartReducer,
-        wishlist: wishlistReducer,
     };
 
     const rootReducer = combineReducers(reducers);
