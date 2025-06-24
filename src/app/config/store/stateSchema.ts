082da2628391695f9fb6aad40a963a9a9b3e757d
Yaroslav Kuibida <126953294+FallenSpacee@users.noreply.github.com>
2025-06-24 21:17:02 +0300

    Revert "feat(scrum-8): added store for wishlist"
    This reverts commit 3ed33f46508382f6bf4e4ccc2bc13364a83f92a1.

--- src/app/config/store/stateSchema.ts
+++ src/app/config/store/stateSchema.ts
@@ -1,7 +1,5 @@
 import type { CartStateType } from '@/features/cart/model/types/cartTypes';
-import type { WishlistStateType } from '@/features/wishlist/model/types/wishlistTypes';
 
 export type StateSchema = {
     cart: CartStateType;
-    wishlist: WishlistStateType;
 };
