a2cf0e405acec96c75c8b4a38c7a220b08eeae91
Dimatuzkoff <dimatuzkoff@gmail.com>
2025-07-02 23:06:58 +0300

    feat(scrum-8): save and load cart store

--- src/entities/wishlist/model/actionCreators/wishlistActionCreators.ts
+++ src/entities/wishlist/model/actionCreators/wishlistActionCreators.ts
@@ -29,7 +29,7 @@ const clearWishlist = (): ClearWishlistType => {
     };
 };
 
-export const cartActionCreators = {
+export const wishlistActionCreators = {
     addProductToWishlist,
     deleteProductFromWishlist,
     clearWishlist,
