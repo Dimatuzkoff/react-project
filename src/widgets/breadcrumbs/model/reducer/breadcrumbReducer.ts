d496a03e57e5106526ef2d9e9d3f505e2067d382
DariaZhur <dariazhur89@gmail.com>
2025-07-05 13:20:57 +0300

    fix(scrum-7): fix initial state

--- src/widgets/breadcrumbs/model/reducer/breadcrumbReducer.ts
+++ src/widgets/breadcrumbs/model/reducer/breadcrumbReducer.ts
@@ -3,29 +3,38 @@ import type { Breadcrumb } from '../types/breadcrumbTypes';
 import type { BreadcrumbActions } from '../types/breadcrumbAction';
 import { BreadcrumbActionTypes } from '../actionTypes/breadcrumbActionTypes';
 
-export type BreadcrumbState = Breadcrumb[];
+export type BreadcrumbsStateType = {
+    breadcrumbs: Breadcrumb[];
+};
 
-const initialState: BreadcrumbState = [];
+const initialState: BreadcrumbsStateType = {
+    breadcrumbs: [],
+};
 
-export const breadcrumbReducer: Reducer<BreadcrumbState, BreadcrumbActions> = (
-    state = initialState,
-    action
-) => {
+export const breadcrumbReducer: Reducer<
+    BreadcrumbsStateType,
+    BreadcrumbActions
+> = (state = initialState, action) => {
     switch (action.type) {
-        case BreadcrumbActionTypes.SET_BREADCRUMBS: {
-            return [...action.payload];
-        }
+        case BreadcrumbActionTypes.SET_BREADCRUMBS:
+            return {
+                ...state,
+                breadcrumbs: [...action.payload],
+            };
 
-        case BreadcrumbActionTypes.ADD_BREADCRUMB: {
-            return [...state, action.payload];
-        }
+        case BreadcrumbActionTypes.ADD_BREADCRUMB:
+            return {
+                ...state,
+                breadcrumbs: [...state.breadcrumbs, action.payload],
+            };
 
-        case BreadcrumbActionTypes.CLEAR_BREADCRUMBS: {
-            return [];
-        }
+        case BreadcrumbActionTypes.CLEAR_BREADCRUMBS:
+            return {
+                ...state,
+                breadcrumbs: [],
+            };
 
-        default: {
+        default:
             return state;
-        }
     }
 };
