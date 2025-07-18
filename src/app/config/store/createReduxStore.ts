99a7adc06ce147e001c9777eba9935a41c848061
Dimatuzkoff <dimatuzkoff@gmail.com>
2025-07-18 22:50:34 +0300

    feat(scrum-8): conflict resolved

--- src/app/config/store/createReduxStore.ts
+++ src/app/config/store/createReduxStore.ts
@@ -2,17 +2,29 @@ import { combineReducers, createStore } from 'redux';
 import { cartReducer } from '@/entities/cart/model/reducer/cartReducer';
 import { wishlistReducer } from '@/entities/wishlist/model/reducer/wishlistReducer';
 import { breadcrumbReducer } from '@/widgets/breadcrumbs/model/reducer/breadcrumbReducer';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 export const createReduxStore = () => {
+    const persistConfig = {
+        key: 'root', 
+        storage, 
+        whitelist: ['cart', 'wishlist']
+    };
+
     const reducers = {
         cart: cartReducer,
         wishlist: wishlistReducer,
         breadcrumbs: breadcrumbReducer,
     };
 
-    const rootReducer = combineReducers(reducers);
+    const rootReducer  = combineReducers(reducers);
+    //@ts-expect-error
+    const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-    const store = createStore(rootReducer);
+    const store = createStore(persistedReducer);
 
-    return store;
+    const persistor = persistStore(store);
+
+    return {store, persistor};
 };
