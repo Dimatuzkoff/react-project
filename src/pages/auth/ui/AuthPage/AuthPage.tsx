6299860879703dc526d9fdd9a8cf2c0d4749bde9
Dimatuzkoff <dimatuzkoff@gmail.com>
2025-07-27 16:37:24 +0300

    addea authPage

---
+++ src/pages/auth/ui/AuthPage/AuthPage.tsx
@@ -0,0 +1,20 @@
+//styles
+import styles from './AuthPage.module.scss';
+//ui
+import {AuthForm} from '@/widgets/auth';
+//assets
+import AuthImg from "@/shared/libs/assets/svg/authImg.svg"
+export const AuthPage = () => {
+    return (
+        <>
+            <section className={styles.wrapperAuthPage}>
+                <div className={styles.wrapperAuthImg}>
+                    <img src={AuthImg} alt="authImg" />
+                </div>
+                <div className={styles.wrapperAuthForm}>
+                    <AuthForm /> 
+                </div>
+            </section>
+        </>
+    );
+};
---
+++ src/widgets/auth/ui/AuthFormInput/AuthFormInput.tsx
@@ -0,0 +1,41 @@
+import { useFormContext } from 'react-hook-form';
+//ui
+import { Input } from '@/shared/ui/input';
+import type { FC } from 'react';
+
+interface AuthFormInputProps {
+    isFullWidth?: boolean;
+    helperText?: string;
+    name: string;
+    size?: '24' | '32' | '36' | '40' | '44' | '48';
+    placeholder?: string;
+    tooltipText?: string;
+    type: React.InputHTMLAttributes<HTMLInputElement>['type'];
+    isError?: boolean;
+}
+
+export const AuthFormInput: FC<AuthFormInputProps> = ({ 
+    name,
+    isFullWidth,
+    placeholder,
+    helperText,
+    isError,
+    type, 
+    size  
+}) => {
+    const {
+        register,
+        formState: { errors },
+    } = useFormContext();
+    return (
+        <Input
+            isFullWidth={isFullWidth}
+            size={size}
+            {...register(name)}
+            isError={isError || !!errors[name]}
+            helperText={helperText || errors[name]?.message?.toString()}
+            placeholder={placeholder}
+            type={type}
+        />
+    );
+};
