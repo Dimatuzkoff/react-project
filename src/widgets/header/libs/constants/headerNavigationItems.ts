0d71b6baf1586149de27f84336b13daa783261df
Dimatuzkoff <dimatuzkoff@gmail.com>
2025-07-27 17:16:05 +0300

    feat(feature/auth_page): added zod

--- src/widgets/header/libs/constants/headerNavigationItems.ts
+++ src/widgets/header/libs/constants/headerNavigationItems.ts
@@ -2,7 +2,7 @@ import {
     getHomeRoute,
     getContactRoute,
     getAboutRoute,
-    getSignUpRoute,
+    getAuthRoute,
 } from '@/shared/libs/constants/routes/routes';
 
 export const headerNavigationItems = [
@@ -20,6 +20,6 @@ export const headerNavigationItems = [
     },
     {
         label: 'Зареєструватися',
-        path: getSignUpRoute(),
+        path: getAuthRoute(),
     },
 ];
