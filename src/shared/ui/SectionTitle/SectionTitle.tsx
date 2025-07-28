3f95e8b470bc53fb0648e6614eb526287ae9684a
DariaZhur <dariazhur89@gmail.com>
2025-07-28 18:36:07 +0300

    feat(scrum-10): fix props

--- src/shared/ui/SectionTitle/SectionTitle.tsx
+++ src/shared/ui/SectionTitle/SectionTitle.tsx
@@ -4,7 +4,7 @@ import type {FC} from 'react'
 import styles from './SectionTitle.module.scss'
 
 interface SectionTitleProps {
-  title: string
+  title?: string
 }
 
 export const SectionTitle:FC<SectionTitleProps> = ({title}) => {
