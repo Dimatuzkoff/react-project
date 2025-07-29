e2617de1ff6f03bace87000ec94f921916160a84
DariaZhur <dariazhur89@gmail.com>
2025-07-29 17:17:40 +0300

    feat(scrum-10): fixed wishlist icon

--- src/entities/product/ui/ProductGallery/ProductGallery.tsx
+++ src/entities/product/ui/ProductGallery/ProductGallery.tsx
@@ -2,13 +2,18 @@
 import { useState, type FC } from 'react';
 // styles
 import styles from './ProductGallery.module.scss';
+import clsx from 'clsx';
 
 interface ProductGalleryProps {
     images: string[];
 }
 
 export const ProductGallery: FC<ProductGalleryProps> = ({ images }) => {
-  const [activeImage, setActiveImage] = useState(images?.[0]);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleClick = (index: number) => {
+    setActiveIndex(index);
+  }
 
     return (
         <div className={styles.productGallery}>
@@ -18,15 +23,18 @@ export const ProductGallery: FC<ProductGalleryProps> = ({ images }) => {
                         key={index}
                         src={img}
                         alt={`Thumbnail ${index}`}
-                        className={`${styles.thumbnail} ${img === activeImage ? styles.active : ''}`}
-                        onClick={() => setActiveImage(img)}
+                        className={clsx(
+                            styles.thumbnail,
+                            index === activeIndex && styles.active
+                        )}
+                        onClick={() => handleClick(index)}
                     />
                 ))}
             </div>
 
             <div className={styles.mainImageWrapper}>
                 <img
-                    src={activeImage}
+                    src={images[activeIndex]}
                     alt="Main product"
                     className={styles.mainImage}
                 />
