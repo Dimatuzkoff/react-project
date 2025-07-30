3e5d37480f089d1ba6de86c98b0c9a39fbbd4422
Dimatuzkoff <dimatuzkoff@gmail.com>
2025-07-30 22:22:11 +0300

    feat(feature/auth_page): corrected spaces

--- src/widgets/auth/libs/validation/authValidation.ts
+++ src/widgets/auth/libs/validation/authValidation.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 import { passwordSchema } from '@/shared/libs/validation/passwordSchema';
 import { emailSchema } from '@/shared/libs/validation/emailSchema';
 import { firstNameSchema } from '@/shared/libs/validation/firstNameSchema';
+
 export const signUpSchema = z.object({
     firstName: firstNameSchema,
     email: emailSchema,
