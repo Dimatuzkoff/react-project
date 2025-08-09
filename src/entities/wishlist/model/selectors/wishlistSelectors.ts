fb1c32b9a4fa07ce71313c610fe0974eca94d87e
Dimatuzkoff <dimatuzkoff@gmail.com>
2025-08-09 14:33:10 +0300

    feat(feature/wishlist_page): finished wishlist

--- src/entities/wishlist/model/selectors/wishlistSelectors.ts
+++ src/entities/wishlist/model/selectors/wishlistSelectors.ts
@@ -1,3 +1,7 @@
 import type { StateSchema } from '@/app/config/store/stateSchema';
 
 export const getWishlistState = (state: StateSchema) => state.wishlist;
+
+export const getWishlistStateItemsIds = (state: StateSchema) => {
+    return state.wishlist.wishlist.map((item) => item.id) || [];
+}
