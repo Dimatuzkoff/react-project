f38a3f7f17971ed2d3155ef77e0316b88dfeab3a
Dimatuzkoff <dimatuzkoff@gmail.com>
2025-08-11 23:28:27 +0300

    feat(feature/wishlist_page): show smartPick in console

--- src/entities/smartPick/libs/hooks/useSmartPick.ts
+++ src/entities/smartPick/libs/hooks/useSmartPick.ts
@@ -1,6 +1,8 @@
 // redux
 import { useSelector } from "react-redux";
 import { getViewedProductsState } from '@/entities/smartPick/model/selectors/smartPickSelectors'
+// hooks
+// import { useAddToSmartPick } from '@/entities/smartPick/libs/hooks/smartPickActions'
 // type
 import { type Product } from '@/entities/product/model/types/product';
 // helpers
@@ -10,14 +12,21 @@ import { useProductsByCategory } from '@/entities/smartPick/libs/hooks/useProduc
 export const useSmartPick = async () => {
     const viewedProducts: Product[]  = useSelector(getViewedProductsState);
     const amountPreferProducts = getAmountPreferProducts(viewedProducts.length);
-    const productsByCategoryHook = useProductsByCategory();
+    const productsByCategory = useProductsByCategory();
+    // const addToSmartPick = useAddToSmartPick();
     if ( !amountPreferProducts ) return
+    const smartPick =[];
     for (const category of amountPreferProducts) {
-        const currentCategoryProducts = await productsByCategoryHook(category);
-        
+        const currentCategoryProducts = await productsByCategory(category);
+        smartPick.push(...currentCategoryProducts);
         console.log('Товары для одной категории:', currentCategoryProducts);
         
     }
-console.log('Расчет по просмотренным категори:', amountPreferProducts);
+    if (smartPick.length ===0 ) return  
+    // for (const product of smartPick) {
+    //     addToSmartPick(product);
+    // }
 
+    console.log('СмартПик:', smartPick);
+console.log('Расчет по просмотренным категори:', amountPreferProducts);
 }
